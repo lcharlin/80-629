499e443fe072cc81ad1486f47f37961a99d01681
Laurent Charlin <lcharlin@gmail.com>
2020-09-10 23:03:33 -0400

    week 2 exos (from D. Berger)

---
+++ week2-Fundamentals/utilities.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+
+from sklearn.model_selection import train_test_split
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def data_simulation(sample_size, scale, period, variance):
+    
+    x = np.random.uniform(-scale, scale, sample_size)
+    x.sort()
+    noise = np.random.normal(0, variance, sample_size)
+    y = x**1 * np.cos(x / period) + noise
+    
+    return x, y
+
+def scatter_plot(x_train, x_test, y_train, y_test):
+    plt.figure(figsize=(15,5))
+    plt.plot(x_train, y_train, '.', color='black', markersize= 3, label='train')
+    plt.plot(x_test, y_test, '.', color='red', markersize= 8, label='test')
+
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    leg = plt.legend(loc='lower center', fontsize='large')
+    leg.get_frame().set_alpha(0)
+    return plt
+    
+def plot_polynomial_curves(x_train, x_test, y_train, y_test, degre):
+
+    loss_train_stack, loss_test_stack = [], []
+    color=cm.rainbow(np.linspace(0,1,len(degre)))
+    plt = scatter_plot(x_train, x_test, y_train, y_test)
+
+    for k,c in zip(range(len(degre)),color):
+        coef = np.polyfit(x_train, y_train, degre[k])
+        
+        y_hat_train = np.polyval(coef, x_train)
+        y_hat_test = np.polyval(coef, x_test)
+        
+        loss_train_stack.append(MSE(y_hat_train, y_train))
+        loss_test_stack.append(MSE(y_hat_test, y_test))
+        
+        print('Polynomial degre: ', degre[k], ' | MSE train:', np.round(loss_train_stack[-1], 4), ' | MSE test:', np.round(loss_test_stack[-1], 4))
+        plt.plot(x_train, y_hat_train, color=c, label=degre[k],)
+    
+    leg = plt.gca().legend(loc='center left', bbox_to_anchor=(1, .65), title="Polynomial degre of  \n  the fitted curve \n")
+    leg.get_frame().set_alpha(0)    
+
+    
+def plot_optimal_curve(optimal_train, optimal_test, H_train, H_test, optimal_degre):
+
+    cmap = plt.get_cmap("tab10")   # Because I prefer this color map
+
+    H = np.concatenate((optimal_train, optimal_test, H_train[:,2], H_test[:,2]), axis=0)
+    mini, maxi = min(H), max(H)
+    linewidth = 2
+    fig, ax1 = plt.subplots(figsize=(15,5))
+    ax2 = ax1.twinx()
+    
+    # Home made tricks for great legend
+    ax1.plot([-20] * len(optimal_train),  label='Training', color='k', linewidth=1)
+    ax1.plot([-20] * len(optimal_train), label='Test', linestyle='dashed', color='k', linewidth=1)
+    ax1.plot([-20] * len(optimal_train), label='     ', linestyle='dashed', color='white')
+
+    ax1.plot(H_train[:,2], color=cmap(1), linestyle='dashed', linewidth=linewidth)   # Since we are interested in the cubic curve
+    ax1.plot(H_test[:,2], color=cmap(1), label='Cubic',linewidth=linewidth)
+    
+    ax1.plot(optimal_train, color=cmap(0), linestyle='dashed', linewidth=linewidth)   # Since we are 'also' interested in the optimal curve
+    ax1.plot(optimal_test, color=cmap(0), label='Optimal Capacity', linewidth=linewidth)
+
+    ax2.plot(optimal_degre, color=cmap(2), label='Optimial degree', linewidth=linewidth)   # Optimal degre with respect to the sample size
+
+    plt.xticks([0, 1, 2, 3, 4, 5])
+
+    ax1.set_xlabel('Sample size:  $\ \log_{10}(n) - 1$')
+    ax1.set_ylabel('MSE')
+    ax1.set_ylim(mini-50, maxi+100)
+    ax2.set_ylabel('Degree of the plynomial', fontsize=12, color = 'green')
+    leg1 = ax1.legend(loc='center left', bbox_to_anchor=(1.1, .8))   # Legend location is somehow important to me
+    leg1.get_frame().set_alpha(0)   # Legend without frame > legend with frame imo
+    leg2 = ax2.legend(loc='center left', bbox_to_anchor=(1.1, .8))   # Legend location is somehow important to me
+    leg2.get_frame().set_alpha(0)   # Legend without frame > legend with frame imo
+
+
+def train_poly_and_see(sample_size, scale, period, variance, degre):
+    H_train = np.zeros((len(sample_size), len(degre)))
+    H_test = np.zeros((len(sample_size), len(degre)))
+
+    optimal_train, optimal_test, optimal_degre = [], [], []
+
+    i = 0
+    for n in sample_size:
+
+        x_train, y_train = data_simulation(n, scale, period, variance)
+        x_test, y_test = data_simulation(1000, scale, period, variance)
+
+        j = 0
+        for k in degre:
+            coef = np.polyfit(x_train, y_train, k)
+
+            y_hat_train = np.polyval(coef, x_train)
+            y_hat_test = np.polyval(coef, x_test)
+
+            H_train[i, j] = MSE(y_train, y_hat_train)
+            H_test[i, j] = MSE(y_test, y_hat_test)
+            j += 1
+
+        optimal_degre.append(np.argmin(H_test[i, :]))
+        optimal_train.append(H_train[i, optimal_degre[-1]])
+        optimal_test.append(H_test[i, optimal_degre[-1]])
+        i +=1
+    
+    return H_train, H_test, optimal_train, optimal_test, optimal_degre
+    
+def MSE(a, b):
+    return ((a-b)**2).mean()
