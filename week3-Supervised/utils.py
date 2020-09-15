2b1146ce7affc41224ea574b3c4c47fe0da63aa5
Oleksiy Ostapenko <oleks.ostapenko@icloud.com>
2020-09-15 19:03:48 -0400

    round of improvements

--- week3-Supervised/utils.py
+++ week3-Supervised/utils.py
@@ -13,8 +13,8 @@ def generate_data():
 
     rng = np.random.RandomState(2)
     
-    X_train += 2 * rng.uniform(size=X_train.shape)
-    X_test += 3 * rng.uniform(size=X_test.shape)
+    X_train += 4 * rng.uniform(size=X_train.shape)
+    X_test += 4 * rng.uniform(size=X_test.shape)
 
     linearly_separable_train = (X_train, y_train)
     linearly_separable_test = (X_test, y_test)
@@ -25,19 +25,18 @@ def generate_data():
                 make_circles(noise=0.1, factor=0.5, random_state=1),
                 linearly_separable_train
                 ]
-    datasets_test = [make_moons(noise=0.3, random_state=10,),
-                make_circles(noise=0.3, factor=0.5, random_state=10),
+    datasets_test = [make_moons(noise=0.15, random_state=10,),
+                make_circles(noise=0.15, factor=0.5, random_state=10),
                 linearly_separable_test
                 ]
     return datasets_train, datasets_test
-
 def one_hot(a):
     h = np.zeros((a.size, a.max()+1))
     h[np.arange(a.size),a] = 1
     return h
 
 def plot_predictions(i,X, Y, X_test, Y_test, pred_train, pred_test, line_x=None, line_y=None, plot_svm=None, plot_nb = None):
-    fig = plt.figure(figsize=(15,3))
+    fig = plt.figure(figsize=(12,5))
     
     #TRAIN: indices samples each class
     i_c0 = (Y == 0)
@@ -50,28 +49,17 @@ def plot_predictions(i,X, Y, X_test, Y_test, pred_train, pred_test, line_x=None,
         
     # train
     ax1 = fig.add_subplot(1,2,1)
-    ax1.scatter(X[:,0][i_c0][i_c0_t], X[:,1][i_c0][i_c0_t], marker="+", c='indigo', label='class 0 correct')
-    ax1.scatter(X[:,0][i_c0][i_c0_f], X[:,1][i_c0][i_c0_f], marker="o", c='gold', label='class 0 incorrect')
+    ax1.scatter(X[:,0][i_c0][i_c0_t], X[:,1][i_c0][i_c0_t], marker="+", c='indigo', label='class 0 ')
+    if len(X[:,0][i_c0][i_c0_f])>0:
+        ax1.scatter(X[:,0][i_c0][i_c0_f], X[:,1][i_c0][i_c0_f], marker="o", c='gold', label='class 0 incorrect')
     
-    ax1.scatter(X[:,0][i_c1][i_c1_t],X[:,1][i_c1][i_c1_t], marker="+", c='gold', label='class 1 correct')
-    ax1.scatter(X[:,0][i_c1][i_c1_f],X[:,1][i_c1][i_c1_f], marker="o", c='indigo', label='class 1 incorrect')
+    ax1.scatter(X[:,0][i_c1][i_c1_t],X[:,1][i_c1][i_c1_t], marker="+", c='gold', label='class 1 ')
+    if len(X[:,0][i_c1][i_c1_f])>0:
+        ax1.scatter(X[:,0][i_c1][i_c1_f],X[:,1][i_c1][i_c1_f], marker="o", c='indigo', label='class 1 incorrect')
     ax1.set_title(f'Dataset {i}, Train')
-    ax1.legend(loc='upper left', ncol=2);
-    if plot_svm is not None:
-        plot_svc_decision_function(plot_svm, ax1)
+    #ax1.legend(loc='upper left', ncol=2);
     
-    if plot_nb is not None:
-        plot_nb_decision(plot_nb, ax1)
-    
-    if not line_x is None and not line_y is None:
-        ax1.plot(line_x, line_y)
-    
-    ax1.set_xlim((min(X[:,0]), max(X[:,0])))
-    ax1.set_ylim((min(X[:,1]), max(X[:,1])))
-    
-    
-    
-    
+         
     #TEST: indices samples each class
     i_c0 = (Y_test == 0)
     i_c1 = (Y_test == 1)
@@ -81,17 +69,37 @@ def plot_predictions(i,X, Y, X_test, Y_test, pred_train, pred_test, line_x=None,
     i_c1_t = (pred_test[i_c1]==1); i_c1_f = (pred_test[i_c1]==0)
     # test
     ax2 = fig.add_subplot(1,2,2)
-    ax2.scatter(X_test[:,0][i_c0][i_c0_t], X_test[:,1][i_c0][i_c0_t], marker="+", c='indigo', label='class 0 correct')
-    ax2.scatter(X_test[:,0][i_c0][i_c0_f], X_test[:,1][i_c0][i_c0_f], marker="o", c='gold', label='class 0 incorrect')
+    ax2.scatter(X_test[:,0][i_c0][i_c0_t], X_test[:,1][i_c0][i_c0_t], marker="+", c='indigo', label='class 0 ')
+    if len(X_test[:,0][i_c0][i_c0_f])>0:
+        ax2.scatter(X_test[:,0][i_c0][i_c0_f], X_test[:,1][i_c0][i_c0_f], marker="o", c='gold', label='class 0 incorrect')
     
-    ax2.scatter(X_test[:,0][i_c1][i_c1_t],X_test[:,1][i_c1][i_c1_t], marker="+", c='gold', label='class 1 correct')
-    ax2.scatter(X_test[:,0][i_c1][i_c1_f],X_test[:,1][i_c1][i_c1_f], marker="o", c='indigo', label='class 1 incorrect')
+    ax2.scatter(X_test[:,0][i_c1][i_c1_t],X_test[:,1][i_c1][i_c1_t], marker="+", c='gold', label='class 1 ')
+    if len(X_test[:,0][i_c0][i_c0_f])>0:
+        ax2.scatter(X_test[:,0][i_c1][i_c1_f],X_test[:,1][i_c1][i_c1_f], marker="o", c='indigo', label='class 1 incorrect')
     
     if plot_svm is not None:
         plot_svc_decision_function(plot_svm, ax2)
     
     ax2.set_title(f'Dataset {i }, Test ')
-    ax2.legend(loc='upper left', ncol=2);
+    ax2.legend(bbox_to_anchor=(1.05, 0.5));
+    
+    ax1.set_xlim(   min(min(X[:,0]),min(X_test[:,0]))-0.1,  max(max(X[:,0]),max(X_test[:,0]))+0.1 )
+    ax1.set_ylim(   min(min(X[:,1]),min(X_test[:,1]))-0.1,  max(max(X[:,1]),max(X_test[:,1]))+0.1 )
+    
+    
+    ax2.set_xlim(   min(min(X[:,0]),min(X_test[:,0]))-0.1,  max(max(X[:,0]),max(X_test[:,0]))+0.1 )
+    ax2.set_ylim(   min(min(X[:,1]),min(X_test[:,1]))-0.1,  max(max(X[:,1]),max(X_test[:,1]))+0.1 )
+    
+    if plot_svm is not None:
+        plot_svc_decision_function(plot_svm, ax1)
+    
+    if plot_nb is not None:
+        plot_nb_decision(plot_nb, ax1)
+    
+    if not line_x is None and not line_y is None:
+        ax1.plot(line_x, line_y)
+    
+    
     if not line_x is None and not line_y is None:
         ax2.plot(line_x, line_y)
     
@@ -99,8 +107,6 @@ def plot_predictions(i,X, Y, X_test, Y_test, pred_train, pred_test, line_x=None,
         plot_nb_decision(plot_nb, ax2)
     
     
-    ax2.set_xlim((min(X[:,0]), max(X[:,0])))
-    ax2.set_ylim((min(X[:,1]), max(X[:,1])))
     
 def plot_svc_decision_function(model, ax, plot_support=True):
     """Plot the decision function for a 2D SVC"""
